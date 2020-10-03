0239e32a98d25ba4903c5f781a490382f76e5e28
wyink <country7777iy@gmail.com>
2020-10-03 14:28:00 +0900

    add testCase

--- BioInfoLibrary/BioInfoLibrary.cpp
+++ BioInfoLibrary/BioInfoLibrary.cpp
@@ -6,49 +6,65 @@
 #include "PajekHelper.h"
 #include "BlastParser.h"
 #include <filesystem>
+namespace fs = std::filesystem;
 
+/* TestCase1
+* Faa形式のファイルをFasta形式に変換
+* 必要なIDの塩基配列のみを抽出
+*/
+void main1();
 
+/* TestCase2
+* Blastの結果からAccessionIDの情報を種情報に変換してフォーマットを
+* 整理して出力する
+*/
+void main2();
+
+/* TestCase3
+* F-measureを計算して指定のフォーマットで出力
+*/
+void main3();
+
+/* TestCase4
+* F-measureの結果からPajek形式のファイルを作成
+*/
+void main4();
 
 int main() {
-    namespace fs = std::filesystem;
 
-    /* Case1
-    * Faa形式のファイルをFasta形式に変換
-    * 必要なIDの塩基配列のみを抽出
-    */
-    /*
+    main4();
+
+}
+
+void main1() {
     std::string faafile = "D:/perflingens/2_protein/GCA_000009685.1_ASM968v1_protein.faa";
     std::string out = "D:/perflingens/otameshi.fasta";
 
     FaaToFasta fta(faafile, out);
     fta.setIdFilterCall(
-            [](std::string idLine)->bool {
-                std::string::size_type n = idLine.find("(plasmid)");
-                if (n == std::string::npos) {// 核ゲノム
-                    return true;
-                }
-                else {//見つかったー＞プラスミド
-                    return false;
-                }
+        [](std::string idLine)->bool {
+            std::string::size_type n = idLine.find("(plasmid)");
+            if (n == std::string::npos) {// 核ゲノム
+                return true;
             }
-        )
+            else {//見つかったー＞プラスミド
+                return false;
+            }
+        }
+    )
         .run();
-    */
+}
 
-    /* Case2
-    * Blastの結果からAccessionIDの情報を種情報に変換してフォーマットを
-    * 整理して出力する
-    */
-    
+void main2() {
     //各fastaファイルのAccessionIDとstrainIDの対応mapの作成
-    std::map<std::string,std::string> ac_st_map = Utils::keyValMakeFromFile(
+    std::map<std::string, std::string> ac_st_map = Utils::keyValMakeFromFile(
         "D:/perflingens/new_resources/5_prid_strain.txt",/**< 入力ファイル */
         "\t",                                            /**< 入力ファイルに使用されているデリミタ */
         [](std::vector<std::string>& vec)->std::vector<std::string>& {
             vec[0] = vec[0].substr(1);/** 先頭文字の'>'は省く。vec[1]（値）は変更しない */
             return vec;
         }
-    ) ;
+    );
 
     //ファイル名（パス）とstrainIDの対応mapの作成
     std::map<std::string, std::string> fpath_stid_map = Utils::keyValMakeFromFile(
@@ -60,7 +76,7 @@ int main() {
             return vec;
         }
     );
-    
+
     //各AccessionIDに対してヒットしたAccessionIDをstrainIDに変換し、再集計したのちに出力
     //各ファイルの1行目にはそのファイルが所属するstrainIDを出力
     fs::path outfile;
@@ -70,9 +86,8 @@ int main() {
     std::smatch sm;
     std::shared_ptr<BlastParserPt1Imple> bpi = std::make_shared<BlastParserPt1Imple>(ac_st_map);
     for (const fs::directory_entry& x : fs::directory_iterator("D:/perflingens/4_blast/result/")) {
-        //BlastParser bp(x.path(), bpi);
-        outfile =  "D:/perflingens/4_blast/analyzedAgain" / x.path().filename();
-        std::cout << outfile << "\n" ;
+        outfile = "D:/perflingens/4_blast/analyzedAgain" / x.path().filename();
+        std::cout << outfile << "\n";
 
         //入力ファイル名をstrainIDに変換して出力ファイルのheaderとして出力
         filename = x.path().filename().string();
@@ -82,28 +97,24 @@ int main() {
         std::make_unique<BlastParser>(x.path(), bpi)->run(outfile, header);
         exit(0);
     }
-  
 
-    /* Case3 
-    * F-measureを計算して指定のフォーマットで出力
-    */
+}
 
-    /*
-    const fs::path indir = "G:/perflingens/4_blast/analyzedAgain";
-    const fs::path out = "G:/perflingens/out.txt";
+void main3() {
 
-    FmeasurePt1 fh(FmeasurePt1::CountUpWay::Exist);
-    Fmeasure fm(indir,out,fh);
+    Fmeasure fm(
+        "D:/perflingens/4_blast/analyzedAgain", /**< 入力ディレクトリ */
+        "D:/perflingens/out.txt",               /**< 出力ファイル */
+        std::make_unique<FmeasurePt1>(FmeasurePt1::CountUpWay::Exist)
+    );
+
     fm.run();
-    */
+}
 
-    /*
-    * F-measureの結果からPajek形式のファイルを作成
-    */
-    /*
-    fs::path dir = "G:/perflingens";
+void main4() {
+    fs::path dir = "D:/perflingens";
     fs::path infile = dir / "out.txt"; /**< 入力ファイル */
-    /*
+
     CreateFromText cft(
         infile,
         std::make_unique<LabelSingle>("Label"),
@@ -112,14 +123,11 @@ int main() {
             return node;
         }
     );
-    
+
     std::vector<std::unique_ptr<Pajek> >  pvec = cft.run();
-    for(auto iter=pvec.begin();iter != pvec.end();++iter){
-        fs::path outfile = dir / "9_pajek_new" ;
+    for (auto iter = pvec.begin(); iter != pvec.end(); ++iter) {
+        fs::path outfile = dir / "9_pajek_new";
         outfile /= (*iter)->getPajekLbel() + ".net"; //string to fs::path
         (*iter)->output(outfile);
     }
-    */
-
 }
-
